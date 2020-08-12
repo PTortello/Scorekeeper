70d39d41dcfeefc36c27ae3a542d4e89fd7302bd
Pedro Tortello <ptortello@hotmail.com>
2020-08-12 16:16:46 -0300

    Py file added

---
+++ scorekeeper.py
@@ -0,0 +1,15 @@
+"""
+Baseball Scorekeeper
+Copyright (C) 2020 - Pedro Tortello
+ptortello@hotmail.com
+"""
+
+
+# TODO: Create main functions. <PTO 2020-08-12 p:3>
+#def team_loader
+#def player_loader
+#def main_menu
+#def team_menu
+#def score_keep
+
+
