46a4cc8b5d91e0b2a938cac1f65e4dcf51fc4b85
Pedro Tortello <ptortello@hotmail.com>
2020-08-13 21:10:29 -0300

    Initial class Player

--- scorekeeper.py
+++ scorekeeper.py
@@ -13,3 +13,18 @@ ptortello@hotmail.com
 #def score_keep
 
 
+class Player:
+    def __init__(self, jersey, name, position):
+        self.jersey = jersey
+        self.name = name
+        self.position = position
+        #stats = [0,0,0,0,0]
+
+player1 = Player('10', 'Pedro Tortello', 'CF')
+print(player1.jersey + ' ' + player1.name + '\t' + player1.position)
+
+
+position = {'DH':0, 'P':1, 'C':2, '1B':3, '2B':4,
+            '3B':5, 'SS':6, 'LF':7, 'CF':8, 'RF':9}
+
+print(position.get(player1.position))
